fa09d7b84a07f0959f95a2aa946e422060eb98d5
Cameron <cameron.bobo02@gmail.com>
2019-04-22 22:08:31 -0400

    T2DM and 3001 Update
    Update to the analysis for the T2DM code in preparation for paper submission. Update to the Sec. 3001 code as part of the paper writing process.

--- PatientExperienceData/PED_analysis.R
+++ PatientExperienceData/PED_analysis.R
@@ -1,12 +1,14 @@
 library(plyr)
 library(dplyr)
 library(ggplot2)
+library(tidyr)
+library(stringr)
 
 # ------------------------------------**Load and Format Data**---------------------------------
 
 setwd("~/GitHub/Data-Driven-Policy/PatientExperienceData") #set working directory
 
-# Data extracted from 2018 CDER New Drug Approval Document (https://www.fda.gov/drugs/developmentapprovalprocess/druginnovation/ucm592464.htm)
+#Data extracted from 2018 CDER New Drug Approval Document (https://www.fda.gov/drugs/developmentapprovalprocess/druginnovation/ucm592464.htm)
 nov_app <- read.csv("2019-01-31_novel_approvals_withBW.csv", header = TRUE)
 nov_app$SubmissionStatusDate <- as.Date(nov_app$SubmissionStatusDate, format = "%m/%d/%Y")
 
@@ -35,12 +37,24 @@ app_PED$BOX1[app_PED$BOX1 > 0] <-1
 app_PED$BOX13 <- app_PED$BOX13 + app_PED$BOX14 + app_PED$BOX15 + app_PED$BOX16 + app_PED$BOX17
 app_PED$BOX13[app_PED$BOX13 > 0] <-1
 
+# Adjudicated boxes for three drugs with ambiguous boxes checked
+app_PED[app_PED$DrugName == "TAKHZYRO","BOX8"] <- 0 # move box 8 to box 15 (not submitted by sponsor)
+app_PED[app_PED$DrugName == "TAKHZYRO","BOX15"] <- 1
+app_PED[app_PED$DrugName == "PALYNZIQ","BOX14"] <- 1 # box 13 indicates there was a PKU Alliance meeting 
+app_PED[app_PED$DrugName == "PALYNZIQ","BOX15"] <- 1 # there was a Voice of the Patient Report
+app_PED[app_PED$DrugName == "XERAVA","BOX5"] <- 1 # COA is marked but the specific COA was not indicated. It is a ClinRO based on analysis of the document.
+
 # ------------------------------------**Novel Drug Analysis**---------------------------------
 app_PED_NME <- app_PED %>% filter(NME == 1) %>% 
   filter(as.Date(SubmissionStatusDate, format = "%m/%d/%Y") >= as.Date("2018-01-01")) %>% 
   mutate(ER = FT + BTD + PR + AA) %>% 
   mutate(num_box = BOX1 + BOX2 + BOX3 + BOX4 + BOX5 + BOX6 + BOX7 + BOX8 + BOX9 + BOX10 + BOX11 
-         + BOX12 + BOX13 + BOX14 + BOX15 + BOX16 + BOX17 + BOX18)
+         + BOX12 + BOX13 + BOX14 + BOX15 + BOX16 + BOX17 + BOX18) %>%
+  mutate(term_box = BOX3 + BOX4 + BOX5 + BOX6 + BOX7 + BOX8 + BOX9 + BOX10 + BOX11
+        + BOX12 + BOX14 + BOX15 + BOX16 + BOX17 + BOX18) %>% #terminal box sum, excluding boxes 1,2,13
+  mutate(SectionA_notCOA = BOX7 + BOX8 + BOX9 + BOX10 + BOX11 + BOX12) # Non-COA parts of section A variable
+
+write.csv(app_PED_NME, "2019_04-18_formatted_PED_output.csv")
 
 nrow(app_PED_NME) # 59 novel drugs
 
@@ -76,8 +90,8 @@ BW_PED <- app_PED_NME[app_PED_NME$BW == 1, ] %>% filter(BOXNA == 0) # Boxed Warn
 app_PED_NME %>% arrange(desc(num_box)) %>%                    #drug with most boxes marked (Crysvita, 8)
    select(DrugName, num_box) %>% head() 
 
-app_PED_NME %>% filter(BOXNA == 0 & BOX18 == 0) %>%           # find mean number of boxes checked (3.6)
-  select(num_box) %>% colMeans()
+app_PED_NME %>% filter(BOXNA == 0 & BOX18 == 0) %>%           # find mean number of terminal boxes marked (1.65)
+  select(term_box) %>% colMeans()
 
 # Patient Reported Outcomes
 
@@ -99,27 +113,95 @@ app_PED_NME %>% filter(BOXNA == 0) %>%                # calculate Box column sum
   select(13:30) %>% colSums()
 
 # Calculate counts for each box broken down by FDA Office
-aggregate(app_PED_NME[app_PED_NME$BOXNA == 0,13:30], by = list(app_PED_NME[app_PED_NME$BOXNA ==0,]$OFFICE), sum)
+office.df <- aggregate(app_PED_NME[app_PED_NME$BOXNA == 0,13:30], by = list(app_PED_NME[app_PED_NME$BOXNA ==0,]$OFFICE), sum)
+office_tally <- app_PED_NME %>% filter(BOXNA == 0) %>% # Total counts for each FDA Office
+  select(OFFICE) %>% table() %>% as.data.frame()
 
-app_PED_NME %>% filter(BOXNA == 0) %>% # Total counts for each FDA Office
-  select(OFFICE) %>% table()
+# Calculate counts for each box broken down by FDA Division
+DOP_PED_NME <- app_PED_NME
+DOP_PED_NME$DIVISION <- str_replace(app_PED_NME$DIVISION, "DOP\\S", "DOP") #combine DOP1 and DOP2
+division.df <- aggregate(app_PED_NME[DOP_PED_NME$BOXNA == 0,13:30], by = list(DOP_PED_NME[app_PED_NME$BOXNA ==0,]$DIVISION), sum)
+division_tally <- DOP_PED_NME %>% filter(BOXNA == 0) %>% # Total counts for each FDA Division
+  select(DIVISION) %>% table() %>% as.data.frame()
 
 # ------------------------------------**Plots**---------------------------------
 
 # Histogram of the number of boxes marked for each drug
 g1 <- (ggplot(app_PED_NME[app_PED_NME$BOXNA == 0 & app_PED_NME$BOX18 == 0, ], aes(x = num_box)) 
-+ geom_histogram(binwidth = 1, color="black", fill="darkblue", alpha = 0.5)
-+ geom_vline(aes(xintercept=mean(num_box)),color="orange", linetype="dashed", size=1.5)
-+ labs(title = "Distribution of Number of Marked Boxes")
-+ xlab("Count")
-+ ylab("Number of Boxes Marked")
-+ scale_y_continuous(breaks = 1:16)
-+ scale_x_continuous(breaks = c(1:8, round(mean(app_PED_NME[app_PED_NME$BOXNA == 0 & app_PED_NME$BOX18 == 0, ]$num_box),1)))
-+ theme_bw()
-+ theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), 
-        axis.line = element_line(colour = "black"), panel.border = element_blank(),
-        axis.text = element_text(size=14), axis.title = element_text(size=16), 
-        plot.title = element_text(size = 24), legend.text = element_text(size = 12),
-        legend.title = element_text(size = 14))
+      + geom_histogram(binwidth = 1, color="black", fill="darkblue", alpha = 0.5)
+      + geom_vline(aes(xintercept=mean(num_box)),color="orange", linetype="dashed", size=1.5)
+      + labs(title = "Distribution of Number of Marked Boxes")
+      + xlab("Count")
+      + ylab("Number of Boxes Marked")
+      + scale_y_continuous(breaks = 1:16)
+      + scale_x_continuous(breaks = c(1:8, round(mean(app_PED_NME[app_PED_NME$BOXNA == 0 & app_PED_NME$BOX18 == 0, ]$num_box),1)))
+      + theme_bw()
+      + theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), 
+              axis.line = element_line(colour = "black"), panel.border = element_blank(),
+              axis.text = element_text(size=14), axis.title = element_text(size=16), 
+              plot.title = element_text(size = 24), legend.text = element_text(size = 12),
+              legend.title = element_text(size = 14))
 )
 g1
+
+# Histogram of the number of terminal boxes marked for each drug
+g2 <- (ggplot(app_PED_NME[app_PED_NME$BOXNA == 0 & app_PED_NME$BOX18 == 0, ], aes(x = term_box)) 
+       + geom_histogram(binwidth = 1, color="black", fill="darkblue", alpha = 0.5)
+       + geom_vline(aes(xintercept=mean(term_box)),color="orange", linetype="dashed", size=1.5)
+       + labs(title = "Distribution of Number of Terminal Marked Boxes")
+       + xlab("Count")
+       + ylab("Number of Terminal Boxes Marked")
+       + scale_y_continuous(breaks = 1:16)
+       + scale_x_continuous(breaks = c(1:8, round(mean(app_PED_NME[app_PED_NME$BOXNA == 0 & app_PED_NME$BOX18 == 0, ]$term_box),2)))
+       + theme_bw()
+       + theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), 
+               axis.line = element_line(colour = "black"), panel.border = element_blank(),
+               axis.text = element_text(size=14), axis.title = element_text(size=16), 
+               plot.title = element_text(size = 24), legend.text = element_text(size = 12),
+               legend.title = element_text(size = 14))
+)
+g2
+
+
+# format data for heatmap plot
+heatmap.office <- as.data.frame(office.df) %>% gather(BOX, NUMBER, -Group.1)
+heatmap.division <- as.data.frame(division.df) %>% gather(BOX, NUMBER, -Group.1)
+
+names(office_tally) <- c("OFFICE", "FREQ")
+heatmap.office <- left_join(heatmap.office, office_tally, by = c("Group.1" = "OFFICE") )
+heatmap.office <- heatmap.office %>% mutate(PERCENT = NUMBER/FREQ*100) %>% filter(Group.1 != "OHOP")
+
+names(division_tally) <- c("DIVISION", "FREQ")
+heatmap.division <- left_join(heatmap.division, division_tally, by = c("Group.1" = "DIVISION") )
+heatmap.division <- heatmap.division %>% mutate(PERCENT = NUMBER/FREQ*100) %>% filter(Group.1 == "DOP" | Group.1 == "DHP")
+heatmap.df <- rbind(heatmap.office, heatmap.division) %>% select(-FREQ)
+heatmap.df$BOX <- as.numeric(str_remove(heatmap.df$BOX, "BOX"))
+
+heatmap.df <- heatmap.df %>% filter(NUMBER > 0) %>%
+  filter(BOX != 1) %>% filter(BOX != 2) %>% filter(BOX != 13) # remove zero counts and non-terminal boxes
+# reorder levels for plotting
+heatmap.df$Group.1 <- factor(heatmap.df$Group.1, levels = c("DOP", "DHP", "ODE3", "ODE2", "ODE1", "OAP"))
+
+# plot "heat map" diagram
+h <- ggplot(heatmap.df, aes(x = BOX, y = Group.1))
+h1 <- (h + geom_point(aes(size = NUMBER, color = PERCENT))
+       + geom_vline(aes(xintercept=13),color="black", size=1)
+       + geom_vline(aes(xintercept=17.5),color="black", size=1)
+       + geom_vline(aes(xintercept=6.5),color="black", size=1, linetype = "dashed")
+       + theme_bw()
+       + scale_size_continuous(breaks = c(1,3,5,7,9), range = c(3, 13))
+       + scale_color_gradient(low = "#ffc547", high = "#0072b2")
+       + scale_x_continuous(breaks = c(3,4,5,6,7,8,9,10,11,12,14,15,16,17,18), minor_breaks = NULL)
+       + labs(title = "Terminal Marked Boxes by Office")
+       + xlab("Box Number")
+       + ylab("Office or Division")
+       + theme(axis.line = element_line(colour = "black"), panel.border = element_blank(),
+               axis.text = element_text(size=14), axis.title = element_text(size=16), 
+               plot.title = element_text(size = 24), legend.text = element_text(size = 12),
+               legend.title = element_text(size = 14))
+       #+coord_flip() # For vertical plot
+       #+ scale_x_reverse(breaks = c(3,4,5,6,7,8,9,10,11,12,14,15,16,17,18), minor_breaks = NULL)
+       
+)
+h1
+
--- T2DM_CVOT/T2DM_yoy.R
+++ T2DM_CVOT/T2DM_yoy.R
@@ -10,9 +10,9 @@ source("CTfunctions.R")
 
 # list of filenames downloaded from ClinicalTrials.gov
 f <- c("Data/2019-01-14-T2DM_clinical_trials_interventional_2000-2017.csv", 
-       "Data/2018-11-12-hypertension_clinical_trials_interventional.csv",
+       "Data/2019-03-28-hypertension_clinical_trials_interventional_2000-2017.csv",
        "Data/2019-01-22-bcancer_clinical_trials_interventional_2000-2017.csv",
-       "Data/2018-11-12-obesity_clinical_trials_interventional.csv",
+       "Data/2019-03-28-obesity_clinical_trials_interventional_2000-2017.csv",
        "Data/2019-01-22-depression_clinical_trials_interventional_2000-2017.csv")
 
 # run the import/formatting function on each dataset
@@ -29,8 +29,8 @@ f_all$DISEASE <- revalue(f_all$DISEASE, c("BREAST_CANCER" = "Breast Cancer", "DI
 f_all %>% filter(INDUSTRY == TRUE) %>% group_by(START_DATE, DISEASE) %>% tally() -> f_diseases_ind # select industry
 f_all %>% filter(INDUSTRY == FALSE) %>% group_by(START_DATE, DISEASE) %>% tally() -> f_diseases_nind # select non-industry
 
-f_diseases_ind$START_DATE <- as.Date(paste0(f_diseases_ind$START_DATE, "-01-01"))
-f_diseases_nind$START_DATE <- as.Date(paste0(f_diseases_nind$START_DATE, "-01-01"))
+# f_diseases_ind$START_DATE <- as.Date(paste0(f_diseases_ind$START_DATE, "-01-01"))
+# f_diseases_nind$START_DATE <- as.Date(paste0(f_diseases_nind$START_DATE, "-01-01"))
 
 # bind hand extracted total intervetional clinical trials (phase 1,2,3) for industry 
 # and non-industry to f_diseases data.frames
@@ -53,7 +53,7 @@ g1 <- (ggplot(f_diseases_ind1, aes(x=START_DATE, y = n))
        + geom_vline(aes(xintercept=as.Date("2008-12-01")), 
                     color = "blue", linetype="dashed", size = 1 )
        + scale_x_date(date_breaks = "1 year", date_labels = "'%y")
-       + labs(title = "Industry T2DM Clinical Trials")
+       + labs(title = "Industry-Sponsored T2DM Clinical Trials")
        + xlab("Trial Start Year")
        + ylab("Number of Clinical Trials")
        + ylim(0,max(f_diseases_ind1$n))
@@ -69,7 +69,7 @@ g2 <- (ggplot(f_diseases_ind2, aes(x=START_DATE, y = n))
        + geom_vline(aes(xintercept=as.Date("2008-12-01")), 
                     color = "blue", linetype="dashed", size = 1 )
        + scale_x_date(date_breaks = "1 year", date_labels = "'%y")
-       + labs(title = "All Industry Clinical Trials")
+       + labs(title = "All Industry-Sponsored Clinical Trials")
        + xlab("Trial Start Year")
        + ylab("Number of Clinical Trials")
        + ylim(0,max(f_diseases_ind2$n))
@@ -149,3 +149,71 @@ g5 <- (ggplot(patents, aes(x=YEAR, y = Type2Diabetes))
                legend.title = element_text(size = 14))
 )
 g5
+
+# plot of clinical trial data by phase
+f_all %>% filter(INDUSTRY == TRUE) %>% group_by(START_DATE, DISEASE, PHASES) %>% tally() -> f_diseases_phases
+f_diseases_phases %>% filter(DISEASE == "Diabetes") -> f_diseases_phases1
+g6 <- (ggplot(f_diseases_phases1, aes(x=START_DATE, y = n, color = PHASES)) 
+       + geom_line(size = 1.3)
+       + geom_vline(aes(xintercept=as.Date("2008-12-01")), 
+                    color = "blue", linetype="dashed", size = 1 )
+       + scale_x_date(date_breaks = "1 year", date_labels = "'%y")
+       + labs(title = "Industry-Sponsored T2DM Clinical Trials by Phase")
+       + xlab("Trial Start Year")
+       + ylab("Number of Clinical Trials")
+       + ylim(0,max(f_diseases_phases1$n))
+       + theme_bw()
+       + scale_colour_brewer(palette = "Set2")
+       + theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), 
+               axis.line = element_line(colour = "black"), panel.border = element_blank(),
+               axis.text = element_text(size=14), axis.title = element_text(size=16), 
+               plot.title = element_text(size = 24), legend.text = element_text(size = 12),
+               legend.title = element_text(size = 14))
+)
+g6
+
+# Plot by therapeutic area, including non diabetes drugs
+f_diseases_ind %>% filter(DISEASE != "All Trials") -> f_diseases_TA
+g7 <- (ggplot(f_diseases_TA, aes(x=START_DATE, y = n, color = DISEASE)) 
+       + geom_line(size = 1.3)
+       + geom_line(data = f_diseases_TA[f_diseases_TA$DISEASE == "Diabetes",], size = 2) # bold diabetes line
+       + geom_vline(aes(xintercept=as.Date("2008-12-01")), 
+                    color = "blue", linetype="dashed", size = 1 )
+       + scale_x_date(date_breaks = "1 year", date_labels = "'%y")
+       + labs(title = "Industry-Sponsored Clinical Trials by Therapeutic Area")
+       + xlab("Trial Start Year")
+       + ylab("Number of Clinical Trials")
+       + ylim(0,max(f_diseases_TA$n))
+       + theme_bw()
+       + scale_colour_brewer(palette = "Set2")
+       + theme(panel.grid.minor = element_blank(), panel.grid.major.x = element_blank(), 
+               axis.line = element_line(colour = "black"), panel.border = element_blank(),
+               axis.text = element_text(size=14), axis.title = element_text(size=16), 
+               plot.title = element_text(size = 24), legend.text = element_text(size = 12),
+               legend.title = element_text(size = 14))
+)
+g7
+
+f_diseases_phases %>% 
+        filter(PHASES == "Phase 1") %>%
+        yoy_stats(time.period = "PRE2008")
+
+f_diseases_phases %>% 
+        filter(PHASES == "Phase 2") %>%
+        yoy_stats(time.period = "PRE2008")
+
+f_diseases_phases %>% 
+        filter(PHASES == "Phase 3") %>%
+        yoy_stats(time.period = "PRE2008")
+
+as_tibble(f_all) %>%
+        filter(INDUSTRY == TRUE & DISEASE == "Diabetes") -> test
+
+# Find enrollment statistics
+test$ENROLLMENT <- as.numeric(levels(test$ENROLLMENT))[test$ENROLLMENT]
+mean(test$ENROLLMENT, na.rm = TRUE)
+sd(test$ENROLLMENT, na.rm = TRUE)/sqrt(sum(!is.na(test$ENROLLMENT)))
+
+test %>%
+        select(PHASES, ENROLLMENT) %>% 
+        aggregate(. ~ PHASES, ., FUN = function(x){sd(x)/sqrt(length(x))})
